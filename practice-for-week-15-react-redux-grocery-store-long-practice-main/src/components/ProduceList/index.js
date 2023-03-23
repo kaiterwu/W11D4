6082da8cee328656c0a0dbaa9b56811a5c418743
Kevin Chan <chankevin13@gmail.com>
2023-03-23 14:56:15 -0400

    Selector

--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/components/ProduceList/index.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/components/ProduceList/index.js
@@ -1,3 +1,6 @@
 import ProduceList from './ProduceList';
+// import { useSelector } from 'react-redux';
+
+// const produce = useSelector(state => state.produce);
 
 export default ProduceList;
--- practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice-main/src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import { populateProduce } from './store/produce';
 
 const store = configureStore();
 if (process.env.NODE_ENV !== "production") {
   window.store = store;
-  
+  window.populateProduce = populateProduce;
 }
+
 function Root() {
   return (
       <Provider store ={store}>
